1114285876af5e716ab430e1d485ca8ab5f3ba89
adikarthik <33541222+adikarthik@users.noreply.github.com>
2026-01-17 03:02:12 +0000

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,7 +1,7 @@
 terraform { 
   cloud { 
     
-    organization = "akg31" 
+    organization = "B12" 
 
     workspaces { 
       name = "localfile" 
@@ -19,10 +19,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-tenant_id="66573a45-6f85-4878-bebc-e0bc24647836"
-subscription_id="5d1b700e-5c37-4a48-a430-e148b56e5404"
-client_secret="PD48Q~IcqNz63VqrTNQKhRMQtejI49K0pG4V1aoR"
-client_id="765f97a9-6831-48a7-9c54-7c8b76af5375"
+client_id="0e15bc0c-2919-4259-8866-3147cdd9100c"
+                        client_secret="Y4p8Q~15bRQgI6dVPfjy~g~2iYQj3sJSlHsVAdk9"
+                        tenant_id="60fb6db9-dc95-4fc8-b91b-2ca9c1b8971f"
+                        subscription_id="c46beff3-d22c-4a6f-82a7-693fd94c69c4"
 }
 variable "rgname" {
   default = "teraform"
