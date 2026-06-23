9bc9b7bbc8520add887786e76b532b84a8e832ff
adikarthik <33541222+adikarthik@users.noreply.github.com>
2026-06-23 07:57:29 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -19,10 +19,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-client_id       = "895046f8-297d-45a6-93a2-a4840da7c2b3"
-  client_secret   = "-0B8Q~TnHGhRemwkCufNGnWlJGYolfTU0aZ5Hdh."
-  tenant_id       = "6ec1ec76-f2c0-493c-b014-6b5497983bea"
-  subscription_id = "20a282cd-41fe-411d-a64e-1727fb143505"
 }
 variable "rgname" {
   default = "teraform1234"
