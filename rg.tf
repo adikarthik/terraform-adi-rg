5b72a3f2772ff0e25a359dbd9fd6068f1574e91f
adikarthik <33541222+adikarthik@users.noreply.github.com>
2026-06-23 07:32:09 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -19,10 +19,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-client_id="0e15bc0c-2919-4259-8866-3147cdd9100c"
-                        client_secret="Y4p8Q~15bRQgI6dVPfjy~g~2iYQj3sJSlHsVAdk9"
-                        tenant_id="60fb6db9-dc95-4fc8-b91b-2ca9c1b8971f"
-                        subscription_id="c46beff3-d22c-4a6f-82a7-693fd94c69c4"
+client_id       = "895046f8-297d-45a6-93a2-a4840da7c2b3"
+  client_secret   = "-0B8Q~TnHGhRemwkCufNGnWlJGYolfTU0aZ5Hdh."
+  tenant_id       = "6ec1ec76-f2c0-493c-b014-6b5497983bea"
+  subscription_id = "20a282cd-41fe-411d-a64e-1727fb143505"
 }
 variable "rgname" {
   default = "teraform"
